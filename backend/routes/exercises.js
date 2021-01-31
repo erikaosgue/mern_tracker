9fb6204829bbee8d9e65203cb5af6858290df8e8
erikaosgue <1476@holbertonschool.com>
2021-01-30 22:34:58 -0500

    Fix Json message when deletes one exercise

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -32,4 +32,35 @@ router.route('/add').post((req, res) => {
     .catch(err => res.status(400).json('Error: '+ err))
 });
 
+// return the exercise by id
+router.route('/:id').get((req, res) => {
+  Exercise.findById(req.params.id)
+  .then(exercise => res.json(exercise))
+  .catch(err => res.status(400).json('Error: ' + err))
+});
+
+// delete the exercise by id
+router.route('/:id').delete((req, res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+  .then(() => res.json('Exercise deleted.'))
+  .catch(err => res.status(400).json('Error: ' + err))
+})
+
+// update an exercise by id
+router.route('/update/:id').post((req, res) => {
+  Exercise.findById(req.params.id)
+  .then(exercise => {
+    exercise.username = req.body.username;
+    exercise.description = req.body.description;
+    exercise.duration = Number(req.body.duration);
+    exercise.date = Date.parse(req.body.date);
+
+    //save the exercise with new information
+    exercise.save()
+    .then(() => res.json('Exercise updated!'))
+    .catch(err => res.status(400).json('Error: ' + err))
+  })
+  .catch(err => res.status(400).json('Error: ' + err))
+})
+
 module.exports = router;
