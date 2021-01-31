663565fd0c571d3607e8f9866d54c29a68e10db6
erikaosgue <1476@holbertonschool.com>
2021-01-30 23:58:27 -0500

    Update new_files to front-end

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from 'react'
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import 'boostrap/dist/css/boostrap.min.css'
 
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create/exercise.component";
+import CreateUser from "./components/create-user.component";
+
+
 
 // the App will return the information to index.js
 function App() {
