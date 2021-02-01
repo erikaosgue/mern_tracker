6b3a0a7394f0f0ba20f3f2465486567fa7fcaa26
erikaosgue <1476@holbertonschool.com>
2021-01-31 23:46:23 -0500

    Update exercises list components

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
-import {link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-const Exercise = props => {
+const Exercise = props => (
   <tr>
     <td>{props.exercise.username}</td>
     <td>{props.exercise.description}</td>
@@ -12,38 +12,40 @@ const Exercise = props => {
       <Link to={"/edit/"+props.exercise._id}>edit</Link> | <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a>
     </td>
   </tr>
-
-}
+)
 
 export default class ExercisesList extends Component {
-  
   constructor(props) {
     super(props);
 
-    this.deleteExercise = this.deleteExercise.bind(this);
-    
-    this.state = { exercises: []}
+    this.deleteExercise = this.deleteExercise.bind(this)
+
+    this.state = {exercises: []};
   }
 
   componentDidMount() {
     axios.get('http://localhost:5000/exercises/')
-    .then(response => {
-      this.setState({exercises: response.data})
-    })
-    .catch(error => console.log(error));
+      .then(response => {
+        this.setState({ exercises: response.data })
+      })
+      .catch((error) => {
+        console.log(error);
+      })
   }
 
   deleteExercise(id) {
-    axios.delete('http://localhost/5000/exercises/' + id)
-    .then(res => console.log(res.data));
-    
+    axios.delete('http://localhost:5000/exercises/'+id)
+      .then(response => { console.log(response.data)})
+      .catch(error => console.log(error));
+
     this.setState({
-      exercises: this.state.exercises.filter(el => el._id != id)
+      exercises: this.state.exercises.filter(el => el._id !== id)
     })
   }
+
   exerciseList() {
     return this.state.exercises.map(currentexercise => {
-      return <Exercise exercise={currentexercise} deleteExercise={this.deleteExercise} key={currentexercise._id}/>
+      return <Exercise exercise={currentexercise} deleteExercise={this.deleteExercise} key={currentexercise._id}/>;
     })
   }
 
