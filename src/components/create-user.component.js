7e4dbc1af06f7e83afb06587abe2599e85cbacba
erikaosgue <1476@holbertonschool.com>
2021-01-31 23:08:33 -0500

    Add http request to the backed to the file create user

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,11 +1,64 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 export default class CreateUsers extends Component {
+ 
+  constructor(props) {
+    super(props);
+
+    // this will make all <this> words in the methods to refer to the class CreateExercises
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    
+    // Initial state of the component
+    this.state = {
+      username: '',
+    }
+  }
   
+  onChangeUsername(e) {
+
+    // we want all this. to refer to the class CreateExercises
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username
+    }
+    console.log(user);
+
+    axios.post('http://localhost:5000/users/add', user)
+    .then(res => console.log(res.data));
+
+    this.setState({
+      username: ''
+    })
+  }
+
+
   render() {
     return (
       <div>
-        <p>You are on the Create Users component</p>
+        <h3>Create New User</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group"> 
+            <label>Username: </label>
+            <input  type="text"
+                required
+                className="form-control"
+                value={this.state.username}
+                onChange={this.onChangeUsername}
+                />
+          </div>
+          <div className="form-group">
+            <input type="submit" value="Create User" className="btn btn-primary" />
+          </div>
+        </form>
       </div>
     )
   }
