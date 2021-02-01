c56a6668c25274fd389c18d829919f4dc79651ee
erikaosgue <1476@holbertonschool.com>
2021-01-31 22:03:44 -0500

    Update src/App.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { BrowserRouter as Router, Route} from "react-router-dom";
-import 'boostrap/dist/css/boostrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import Navbar from "./components/navbar.component";
+import { BrowserRouter as Router, Route} from "react-router-dom";
+
+import Navbar from "./components/navbar.component"
 import ExercisesList from "./components/exercises-list.component";
 import EditExercise from "./components/edit-exercise.component";
-import CreateExercise from "./components/create/exercise.component";
+import CreateExercise from "./components/create-exercise.component";
 import CreateUser from "./components/create-user.component";
 
 
@@ -14,15 +15,17 @@ import CreateUser from "./components/create-user.component";
 function App() {
   return (
     <Router>
-      <Navbar>
+      <div className="container">
+      <Navbar />
         <br/>
         <Route path="/" exact component={ExercisesList} />
         <Route path="/edit/:id" component={EditExercise} />
         <Route path="/create"  component={CreateExercise} />
         <Route path="/user" component={CreateUser} />
-      </Navbar>
+      </div>
     </Router>
   );
 }
+
 
 export default App;
